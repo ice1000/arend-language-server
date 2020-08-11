80e6b6d7790b59b0f615959925933f58a1ab7d5a
ice1000 <ice1000kotlin@foxmail.com>
2020-08-11 17:32:36 +0800

    [ version ] Bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val projectArend = gradle.includedBuild("Arend")
 group = "org.ice1000.arend.lsp"
-version = "0.2.2"
+version = "0.2.6"
 
 plugins {
   idea
